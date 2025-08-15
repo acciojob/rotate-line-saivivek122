a59f8b3b8026bd44bb7a1fd9ceea7484a9c9c410
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-08-15 05:53:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let line=document.getElementById("line");
+let rotateDeg= 0;
+setInterval(function(){
+	rotateDeg=rotateDeg+2;
+	line.style.transform="rotate("+rotateDeg+"deg)";
+},20)
